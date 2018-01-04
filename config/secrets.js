92f7b9d9631abd345041f093dcf82f474b19bfca
Nathaniel Buechler <natebuechler@gmail.com>
2018-01-03 20:04:03 -0500

    feat: Add JWT token sign/verify/auth

--- config/secrets.js
+++ config/secrets.js
@@ -22,6 +22,6 @@
 module.exports = {
 
   db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
-  sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
+  sessionSecret: process.env.SESSION_SECRET || 'secret',
 
 };
